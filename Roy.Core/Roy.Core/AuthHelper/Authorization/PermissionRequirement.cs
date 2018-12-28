fcd2f218490df955fff6a43b8fd30fae1a9ac973
unknown <roy.formssi@outlook.com>
2018-12-28 10:31:20 +0800

    update .gitignore

---
+++ Roy.Core/Roy.Core/AuthHelper/Authorization/PermissionRequirement.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roy.Core.AuthHelper.JWT
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PermissionRequirement : IAuthorizationRequirement
+    {
+
+    }
+}
